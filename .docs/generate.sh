e448d6c69121fad431fce757cd27b39adfb159d2
Lars Mueller <appgurulars@gmx.de>
2020-11-22 17:16:38 +0100

    Dokumentationsgenerator

---
+++ .docs/generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Docs..."
+cd ..
+pandoc Dokumentation.md -f markdown -t latex --pdf-engine=xelatex -s -o Dokumentation.pdf --include-in-header=.docs/header.tex --lua-filter=.docs/files.lua -V fontsize=12pt -M lang:de
+
+for aufgabe in "a1-Woerter-aufraeumen" "a2-Dreieckspuzzle" "a3-Tobis-Turnier" "a5-Wichteln" ; do
+echo "$aufgabe"
+cd "$aufgabe"
+echo "Build..."
+go build main.go
+echo "Run..."
+cd beispieldaten
+for i in *.txt; do (../main "$i") > "../loesungen/${i}"; done
+cd ..
+echo "Docs..."
+pandoc Dokumentation.md -f markdown -t latex --pdf-engine=xelatex -s -o Dokumentation.pdf --include-in-header=../.docs/header.tex --lua-filter=../.docs/files.lua -V fontsize=12pt -M lang:de
+cd ..
+done
+echo "Zip..."
+zip -r "../bwinf39-runde1.zip" "$(pwd)" > /dev/null
+echo "Done"
